fb7fb31e382d733219b0d9c17b44ae97198af032
Martin Granger <martingranger@Martins-MacBook-Pro.local>
2022-07-06 09:10:29 -0400

    quick update

---
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Java!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
